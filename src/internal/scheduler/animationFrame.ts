e944c3ee6bbc9848e7fe517a71d2b9fd4d0f9cc1
akejy032 <44287681+akejy032@users.noreply.github.com>
2018-10-19 16:12:29 +0800

    123
    123

--- src/internal/scheduler/animationFrame.ts
+++ src/internal/scheduler/animationFrame.ts
@@ -1,6 +1,22 @@
 import { AnimationFrameAction } from './AnimationFrameAction';
 import { AnimationFrameScheduler } from './AnimationFrameScheduler';
 
+var observable = Rx.Observable.create(function (observer) {
+    observer.next(1);
+    observer.next(2);
+    observer.next(3);
+    observer.complete();
+});
+
+console.log('before subscribe');
+observable.observeOn(Rx.Scheduler.async) // 設為 async
+.subscribe({
+    next: (value) => { console.log(value); },
+    error: (err) => { console.log('Error: ' + err); },
+    complete: () => { console.log('complete'); }
+});
+console.log('after subscribe');
+
 /**
  *
  * Animation Frame Scheduler
